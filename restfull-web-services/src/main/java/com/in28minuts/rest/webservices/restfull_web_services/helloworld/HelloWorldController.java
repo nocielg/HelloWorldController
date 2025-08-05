6b1d1d46003725a65ad3a14c4c9d134d3b7c6b7d
gleis <gleis@LAPTOP-77T0BCEK>
2025-08-05 20:48:13 -0300

    //Primeira versão do código.

---
+++ restfull-web-services/src/main/java/com/in28minuts/rest/webservices/restfull_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.in28minuts.rest.webservices.restfull_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+//Rest API
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
