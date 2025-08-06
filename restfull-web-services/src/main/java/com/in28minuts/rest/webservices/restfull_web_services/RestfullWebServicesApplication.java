1d7a654f338c1d834067f467d49ce5549369786d
gleis <gleis@LAPTOP-77T0BCEK>
2025-08-05 21:18:02 -0300

    // Teste do commit.

--- restfull-web-services/src/main/java/com/in28minuts/rest/webservices/restfull_web_services/RestfullWebServicesApplication.java
+++ restfull-web-services/src/main/java/com/in28minuts/rest/webservices/restfull_web_services/RestfullWebServicesApplication.java
@@ -2,7 +2,7 @@ package com.in28minuts.rest.webservices.restfull_web_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+
 @SpringBootApplication
 public class RestfullWebServicesApplication {
 
