392c728389e8edd99951d24eee98c72a1a678f51
gleis <gleis@LAPTOP-77T0BCEK>
2025-08-13 19:47:18 -0300

    // O código foi aperfeiçoado.

--- restfull-web-services/src/main/java/com/in28minuts/rest/webservices/restfull_web_services/user/UserResource.java
+++ restfull-web-services/src/main/java/com/in28minuts/rest/webservices/restfull_web_services/user/UserResource.java
@@ -1,11 +1,16 @@
 package com.in28minuts.rest.webservices.restfull_web_services.user;
 
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -22,6 +27,21 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id){
-		return service.findOne(id);
+		User user =  service.findOne(id);
+		if(user == null) 
+			throw new UserNotFoundException("id:"+id);
+		
+		
+		return user;
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+												.path("/{id}")
+												.buildAndExpand(savedUser.getId())
+												.toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
