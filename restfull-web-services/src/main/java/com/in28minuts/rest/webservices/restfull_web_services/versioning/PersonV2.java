e74d3d0c4c2b4afd54c6522703a7a089fc722c46
gleis <gleis@LAPTOP-77T0BCEK>
2025-08-18 21:07:41 -0300

    //Implantação do HATEOAS

---
+++ restfull-web-services/src/main/java/com/in28minuts/rest/webservices/restfull_web_services/versioning/PersonV2.java
@@ -0,0 +1,18 @@
+package com.in28minuts.rest.webservices.restfull_web_services.versioning;
+
+public class PersonV2 {
+	private Name name;
+
+	public PersonV2(Name name) {
+		super();
+		this.name = name;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+}
