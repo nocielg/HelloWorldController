436e7a5dc675e5283049a90e0491ec0fd0bfb2c5
gleis <gleis@LAPTOP-77T0BCEK.mshome.net>
2025-09-01 19:27:31 -0300

    //Adicionando o SpringSecurity à aplicação.

---
+++ restfull-web-services/src/main/java/com/in28minuts/rest/webservices/restfull_web_services/security/SpringSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.in28minuts.rest.webservices.restfull_web_services.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+ 
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(
+				auth -> auth.anyRequest().authenticated()		
+		);
+		http.httpBasic(withDefaults());
+		
+		
+		//http.csrf().disable();
+		http.csrf(csrf -> csrf.disable());
+		
+		return http.build();
+	}
+}
